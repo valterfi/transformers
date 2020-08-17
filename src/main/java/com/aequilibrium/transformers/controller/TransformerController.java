06e4f435595c8ae3a2814738b832dd5d6fa51664
Valterfi <morpheuw@gmail.com>
2020-08-17 15:38:46 -0700

    creating model class

---
+++ src/main/java/com/aequilibrium/transformers/controller/TransformerController.java
@@ -0,0 +1,5 @@
+package com.aequilibrium.transformers.controller;
+
+public class TransformerController {
+
+}
---
+++ src/main/java/com/aequilibrium/transformers/model/Transformer.java
@@ -0,0 +1,99 @@
+package com.aequilibrium.transformers.model;
+
+public class Transformer {
+	
+	private String name;
+	
+	private int strength;
+	
+	private int intelligence;
+	
+	private int speed;
+	
+	private int endurance;
+	
+	private int rank;
+	
+	private int courage;
+	
+	private int firepower;
+	
+	private int skill;
+	
+	public Transformer() {
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getStrength() {
+		return strength;
+	}
+
+	public void setStrength(int strength) {
+		this.strength = strength;
+	}
+
+	public int getIntelligence() {
+		return intelligence;
+	}
+
+	public void setIntelligence(int intelligence) {
+		this.intelligence = intelligence;
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public int getEndurance() {
+		return endurance;
+	}
+
+	public void setEndurance(int endurance) {
+		this.endurance = endurance;
+	}
+
+	public int getRank() {
+		return rank;
+	}
+
+	public void setRank(int rank) {
+		this.rank = rank;
+	}
+
+	public int getCourage() {
+		return courage;
+	}
+
+	public void setCourage(int courage) {
+		this.courage = courage;
+	}
+
+	public int getFirepower() {
+		return firepower;
+	}
+
+	public void setFirepower(int firepower) {
+		this.firepower = firepower;
+	}
+
+	public int getSkill() {
+		return skill;
+	}
+
+	public void setSkill(int skill) {
+		this.skill = skill;
+	}
+
+}
