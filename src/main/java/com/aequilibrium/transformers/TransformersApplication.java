5fce1b5ac51d356e29544409d89c632fdb14cdc5
Valterfi <morpheuw@gmail.com>
2020-08-17 14:17:02 -0700

    first commit

---
+++ src/main/java/com/aequilibrium/transformers/TransformersApplication.java
@@ -0,0 +1,13 @@
+package com.aequilibrium.transformers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransformersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransformersApplication.class, args);
+	}
+
+}
